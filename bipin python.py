323724c3ece4984859a9596e5c98a023fa8d8b13
Shub308G <bipink566j@gmail.com>
2025-07-23 16:08:42 +0530

    Add files via upload

---
+++ bipin python.py
@@ -0,0 +1,17 @@
+print ("hello world")
+
+a = 5
+b = 10
+c = 15
+
+if a<b and a<c :
+    print("a is smallest")
+
+elif b<a and b<c :
+    print("b is smallest")
+
+else:
+    print("c is smallest")
+
+for i in range(1,11):
+    print(2*i)
